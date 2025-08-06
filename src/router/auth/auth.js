c2bab27fb106449fb89c9b818e0acc4b1f10a9b8
zahimahmedkhan <zahim1632@gmail.com>
2025-08-06 05:41:18 -0700

    done

--- src/router/auth/auth.js
+++ src/router/auth/auth.js
@@ -112,7 +112,8 @@ appRouter.post("/login", async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: true,
-          sameSite: "None",
+          sameSite: "none",
+          path: "/",
           expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         });
     
